5e696cb9d554722406b26018aa8bd16065c4ca28
Rodrigo Leite <gunisalvo@gmail.com>
2020-05-28 16:13:07 +0100

    data-synthesiser skeleton

---
+++ data-synthesiser/generate_operational_dataset.py
@@ -0,0 +1,11 @@
+from trumania.core import circus
+import trumania.core.random_generators as gen
+import trumania.core.operations as ops
+import trumania.core.story as story
+import trumania.components.time_patterns.profilers as profilers
+import trumania.core.util_functions as util_functions
+
+import pandas as pd
+import sqlalchemy
+
+### generate synthetic data
