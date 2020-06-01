3140743b1699edef0f80a06bf89dfd9cb19b08c8
Rodrigo Leite <gunisalvo@gmail.com>
2020-06-01 12:44:46 +0100

    mock containers

--- test-rig/kyc.py
+++ test-rig/kyc.py
@@ -6,9 +6,13 @@ environment = os.environ.get("TEST_RIG_ENV", "local")
 config = configparser.ConfigParser()
 config.read("config.ini")
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = config["local"]["operationalDB"]
+app.config['SQLALCHEMY_DATABASE_URI'] = config[environment]["operationalDB"]
 
 
 @app.route('/info')
 def info():
-    return "%s ..." % config["local"]["operationalDB"]
+    return "%s ..." % config[environment]["operationalDB"]
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=config[environment].getint("kycPort"))
